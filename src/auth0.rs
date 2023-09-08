d326c17f2414d023b03767ef03ac96388377a954
Jake Meyer <jakewmeyer@gmail.com>
2023-09-08 15:56:11 -0700

    Use internal Auth0 client hashmap instead of creating a new one on jwk load

--- src/auth0.rs
+++ src/auth0.rs
@@ -9,6 +9,8 @@ use std::collections::HashMap;
 
 use crate::error::Error;
 
+/// An Auth0 client that can be used to decode JWT tokens
+/// and call the Auth0 Management API.
 #[derive(Debug, Clone)]
 pub struct Client {
     domain: String,
@@ -17,6 +19,7 @@ pub struct Client {
     jwk_cache: HashMap<String, Jwk>,
 }
 
+/// The claims in the JWT token
 #[derive(Debug, Clone, Deserialize)]
 pub struct AuthClaims {
     pub sub: String,
@@ -33,23 +36,23 @@ impl Client {
         }
     }
 
+    /// Fetch and cache a JWK set from an Auth0 tenant
     pub async fn load_jwk(&mut self) -> Result<(), Error> {
         let url = format!("https://{}/.well-known/jwks.json", self.domain);
         let body = reqwest::get(url).await?.text().await?;
         let jwks: JwkSet = serde_json::from_str(&body)?;
-        let mut jwk_cache = HashMap::new();
         for jwk in jwks.keys {
             let kid = jwk
                 .common
                 .key_id
                 .clone()
                 .ok_or_else(|| anyhow!("No kid found"))?;
-            jwk_cache.insert(kid, jwk.clone());
+            self.jwk_cache.insert(kid, jwk.clone());
         }
-        self.jwk_cache = jwk_cache;
         Ok(())
     }
 
+    /// Validate and decode a JWT token using the cached JWK set
     pub fn decode_token(&self, kid: String, token: &str) -> Result<TokenData<AuthClaims>, Error> {
         if let Some(j) = &self.jwk_cache.get(&kid) {
             match j.algorithm {
@@ -68,6 +71,7 @@ impl Client {
         }
     }
 
+    /// Use the client credentials grant to get a management API access token
     async fn get_management_access_token(&self) -> Result<String, Error> {
         let url = format!("https://{}/oauth/token", self.domain);
         let audience = format!("https://{}/api/v2/", self.domain);
