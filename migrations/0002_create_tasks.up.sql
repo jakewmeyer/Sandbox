fcbbd4e6c328d827cab7f852ee53918a7c593e41
Jake Meyer <jakewmeyer@gmail.com>
2023-11-27 17:56:28 -0800

    Reorder tasks columns to optimize row byte alignment

--- migrations/0002_create_tasks.up.sql
+++ migrations/0002_create_tasks.up.sql
@@ -1,9 +1,16 @@
 CREATE TABLE tasks(
-  id UUID NOT NULL PRIMARY KEY,
-  type TEXT NOT NULL,
-  priority INT NOT NULL DEFAULT 0,
-  payload JSONB NOT NULL,
-  created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-  UNIQUE(type, payload)
+  id BIGINT PRIMARY KEY NOT NULL GENERATED ALWAYS AS IDENTITY,
+  state SMALLINT NOT NULL DEFAULT 0,
+  priority SMALLINT NOT NULL DEFAULT 100 CHECK (priority >= 0),
+  attempt SMALLINT NOT NULL DEFAULT 0 CHECK (attempt >= 0),
+  max_attempts SMALLINT NOT NULL CHECK (max_attempts > 0),
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  attempted_at TIMESTAMPTZ,
+  scheduled_at TIMESTAMPTZ,
+  queue TEXT NOT NULL DEFAULT 'main' CHECK (char_length(queue) > 0 AND char_length(queue) < 128),
+  name TEXT NOT NULL CHECK (char_length(name) > 0 AND char_length(name) < 128),
+  payload JSONB NOT NULL
 );
-CREATE INDEX tasks_priority_idx ON tasks(priority);
+
+CREATE UNIQUE INDEX tasks_row_unique_idx ON tasks USING btree(name, payload);
+CREATE INDEX tasks_fetch_idx ON tasks USING btree(state, queue, priority, scheduled_at, id);
